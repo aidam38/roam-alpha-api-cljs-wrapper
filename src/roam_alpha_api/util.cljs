0420c29ae73eae3d711981b8e58853799a0846bc
Adam Křivka <krivka.adam@gmail.com>
2021-08-30 07:58:25 +0200

    fixed typo

--- src/roam_alpha_api/util.cljs
+++ src/roam_alpha_api/util.cljs
@@ -1,6 +1,6 @@
 (ns roam-alpha-api.util
   (:require [roam-alpha-api.core :refer [api]]))
 
-(def api-util (. api -utils))
+(def api-util (. api -util))
 
 (defn generate-uid [] (. api-util generateUID))
